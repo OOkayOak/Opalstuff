38cb6213cddd2e26fadd3af72350ff06f30fb6d5
Opal <183864149+OOkayOak@users.noreply.github.com>
2026-05-06 00:15:13 +0100

    fix: Opalstuff *should* be fine to use in zip form now

--- main.lua
+++ main.lua
@@ -1,12 +1,13 @@
 -- loader
 OPAL = SMODS.current_mod
 local mod_path = SMODS.current_mod.path
+_nfs = SMODS.NFS or NFS
 
 function OPAL.load_files(path)
-    local files = NFS.getDirectoryItems(mod_path..path)
+    local files = _nfs.getDirectoryItems(mod_path..path)
     for _, filename in ipairs(files) do
         file_path = path.."/"..filename
-        local dir_success, dir_files = pcall(NFS.getDirectoryItems, file_path)
+        local dir_success, dir_files = pcall(_nfs.getDirectoryItems, file_path)
         if dir_success and not(filename:match(".lua$")) then -- this is a directory
             sendTraceMessage('Loading directory '..filename, 'Opalstuff')
             OPAL.load_files(file_path)
